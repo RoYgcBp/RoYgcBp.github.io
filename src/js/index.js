92fbc2e1833908b608122543413a4627b3cf1ec0
RoYgcBp <109206142+RoYgcBp@users.noreply.github.com>
2026-02-16 12:59:54 +0800

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+
+setTimeout(async () => {
+
+    const signInUpBtn = document.getElementById('signIn-Up-Btn');
+    
+    signInUpBtn.addEventListener('click', () => {
+        window.location.href = '/src/html/signIn.html';
+	});
+
+}, 0);
+
